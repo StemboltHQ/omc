34a240763dbe242f0447ce8ade548f61ba8d1802
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2014-10-21 19:42:01 -0700

    Add aws_cred_vault for cred management
    Change-Id: I73ff2f3d7914e0d615acc5212581adc45e04bbc2

--- lib/omc/cli.rb
+++ lib/omc/cli.rb
@@ -1,11 +1,17 @@
 require 'thor'
+require 'aws_cred_vault'
 
 module Omc
   class Cli < Thor
     desc 'ssh', 'Connect to an instance on a stack on an account'
     def ssh(account)
-      puts account
-      raise "please implement me"
+      iam_account = vault.account account
+      puts iam_account.name
+    end
+
+    private
+    def vault
+      AwsCredVault::Toml.new File.join(ENV['HOME'], '.shoelaces')
     end
   end
 end
--- omc.gemspec
+++ omc.gemspec
@@ -18,6 +18,8 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "thor"
+  spec.add_dependency "aws_cred_vault"
+
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
 end
