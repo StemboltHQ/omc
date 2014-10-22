fbb71ea785538debde2e071a09e329afc5a490f4
John Hawthorn <john@freerunningtechnologies.com>
2014-10-21 20:37:55 -0700

    Add better error messages for missing creds
    Change-Id: I3372dc3a19f96458b42677d559f1539502f4d23c

--- lib/omc/cli.rb
+++ lib/omc/cli.rb
@@ -6,14 +6,14 @@ module Omc
   class Cli < Thor
     desc 'ssh', 'Connect to an instance on a stack on an account'
     def ssh(account, stack)
-      iam_account = vault.account account
-      user = iam_account.users.first
+      iam_account = vault.account(account) || abort("Account #{account.inspect} not configured")
+      user = iam_account.users.first || abort("No users configured under #{account.inspect}")
       ops = ::AWS::OpsWorks::Client.new user.credentials
 
       ops_stack = get_by_name ops.describe_stacks[:stacks], stack
       instances = ops.describe_instances(stack_id: ops_stack[:stack_id])[:instances]
       instances.reject!{|i| i[:status] != "online" }
-      instance = instances.first
+      instance = instances.first || abort("No running instances")
       system "ssh #{user.name}@#{instance[:public_ip]}"
     end
 
